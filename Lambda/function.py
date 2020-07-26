d288c1598b1e79c4b3f594cc80f65177e5fe9bdd
Scott Griggs <sgriggs@gmail.com>
2020-07-25 23:15:42 -0400

    Intitial commit

---
+++ Lambda/function.py
@@ -0,0 +1,28 @@
+import json
+import boto3
+
+def handler(event, context):
+    '''When this function is invoked - retrieve visitor count,
+    add one, store in dynamodb, return to client.'''
+    dynamodb = boto3.client('dynamodb')
+
+    # Get Visits
+    response = dynamodb.get_item(
+        TableName='counter', Key={'Site': {'N': '0'}})
+
+    visits = int(response["Item"]["Visits"]["N"]) + 1
+
+    # Store Visits
+    dynamodb.put_item(TableName='counter', Item={
+        'Site': {'N': '0'},
+        'Visits': {'N': str(visits)}
+    })
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(visits),
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+    }
