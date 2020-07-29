70b8e02d482ceeca8aed8fab066d1e2c5171bead
Scott Griggs <sgriggs@gmail.com>
2020-07-29 18:58:43 -0400

    Rewrote function to use dynamodb.resource

--- Lambda/function.py
+++ Lambda/function.py
@@ -1,38 +1,37 @@
-import json
 import boto3
 
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('counter')
 
-def handler(event, context):
-    # When this function is invoked - retrieve visitor count,
-    # add one, store in dynamodb, return to client.
-    dynamodb = boto3.client('dynamodb')
 
-    # Get Visits
-    response = dynamodb.get_item(
-        TableName='counter', Key={'Site': {'N': '0'}})
+def lambda_handler(event, context):
+    response = table.get_item(
+        Key={
+            'Site': 0
+        }
+    )
 
-    # If the item does not exist, create it.
     if 'Item' not in response:
-        dynamodb.put_item(TableName='counter', Item={
-            'Site': {'N': '0'},
-            'Visits': {'N': '0'}
-        })
-        response = dynamodb.get_item(
-            TableName='counter', Key={'Site': {'N': '0'}})
+        # Create Item
+        table.put_item(
+            Item={
+                'Site': 0,
+                'Visits': 0
+            }
+        )
+        # Read table again
+        table.get_item(
+            Key={
+                'Site': 0
+            }
+        )
 
-    visits = int(response["Item"]["Visits"]["N"]) + 1
-
-    # Store Visits
-    dynamodb.put_item(TableName='counter', Item={
-        'Site': {'N': '0'},
-        'Visits': {'N': str(visits)}
-    })
-
+    visits = int(response['Item']['Visits'])
     return {
         'statusCode': 200,
-        'body': json.dumps(visits),
+        'body': visits,
         'headers': {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*'
-        },
+        }
     }
